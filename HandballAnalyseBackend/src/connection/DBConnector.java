d5e573a1931e7df308e18c8402f45bb37592181b
GarryHeinz <47884156+GarryHeinz@users.noreply.github.com>
2022-07-01 18:45:02 +0200

    .

--- HandballAnalyseBackend/src/connection/DBConnector.java
+++ HandballAnalyseBackend/src/connection/DBConnector.java
@@ -1,4 +1,40 @@
 package connection;
 
-public class DBConnector {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnector implements AutoCloseable{
+
+    private final String DB_URL ="jdbc:mysql://localhost:3306/";
+
+    private String DB_USER = "newuser";
+
+    private String DB_PASS = "password";
+
+    private Connection connection;
+
+    public DBConnector(){
+
+    }
+    
+    public void connect() throws SQLException {
+        this.connection = DriverManager.getConnection(DB_URL,DB_USER,DB_PASS);
+    }
+
+    /**
+     *
+     * @return
+     */
+    public boolean insert(){
+        return false;
+    }
+
+
+    @Override
+    public void close() throws Exception {
+        if(!this.connection.isClosed()){
+            this.connection.close();
+        }
+    }
 }
--- HandballAnalyseBackend/src/connection/Main.java
+++ HandballAnalyseBackend/src/connection/Main.java
@@ -1,17 +1,15 @@
 package connection;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/");
-        Statement st = con.createStatement();
-        int result = st.executeUpdate("CREATE DATABASE test");
-        System.out.println(result);
+    public static void main(String[] args) throws SQLException {
+        try(DBConnector db = new DBConnector()){
+            db.connect();
+        }catch(Exception e){
+
+        }
+
     }
 }
